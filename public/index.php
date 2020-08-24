db1333f38705eef0dcb729729bd742ceb6f6be3f
AlxandrV <a.vigneaud@gmail.com>
2020-08-24 11:39:22 +0200

    Add - arborescence

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('../application/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+    'debug' => true,
+    ]);
+    $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$template = $twig->load('base.html.twig');
+echo $template->render(array(
+    'user' => $user,
+    'exemple' => exemple(),
+));
