630ddc6269571d14b4176fef0ac58d21fc2c7ab1
Jeremy <g3raphacs@gmail.com>
2020-08-24 17:00:19 +0200

    router prototype

--- public/index.php
+++ public/index.php
@@ -1,6 +1,33 @@
 <?php
 
 require_once '../vendor/autoload.php';
+
+$router = new AltoRouter();
+
+//Routeur
+$uri = $_SERVER['REQUEST_URI'];
+$router = new AltoRouter();
+$router->setBasePath('../public');
+
+$router->map('GET', '/', function(){
+    echo "page d'accueil";
+});
+
+$router->map('GET', '/contact', function(){
+    echo "page de contact";
+});
+$router->map('GET', '/post-[*:slug]-[i:id]', function($slug, $id){
+    echo "je suis sur l'annonce: $slug qui a l'index: $id";
+});
+$match = $router->match();
+if ($match !== null){
+    call_user_func_array($match['target'], $match['params']);
+}
+
+dump($router);
+dump($match);
+
+//Twig
 // $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
 // $twig = new \Twig\Environment($loader, [
 
@@ -14,4 +41,5 @@ require_once '../vendor/autoload.php';
 //     'user' => $user,
 //     'exemple' => exemple(),
 // ));
-dump($_SERVER);
+
+// dump($_SERVER);
