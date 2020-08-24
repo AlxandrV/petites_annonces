45ec29e8f5da5b134a003cf851c3816cb251f941
Jeremy <g3raphacs@gmail.com>
2020-08-24 21:16:05 +0200

    test autoload composer

--- application/cache/06/06fd9278a5c570b7d6ec9844ffa5a4e9ffcfdf25d8f331d62ca2764ece2b2041.php
+++ application/cache/06/06fd9278a5c570b7d6ec9844ffa5a4e9ffcfdf25d8f331d62ca2764ece2b2041.php
@@ -27,6 +27,8 @@ class __TwigTemplate_ea89abb853b6f80e609215b2e78fc793709b4f1770ec48984d777aa3d9e
         $this->parent = false;
 
         $this->blocks = [
+            'head' => [$this, 'block_head'],
+            'scipt' => [$this, 'block_scipt'],
         ];
     }
 
@@ -37,14 +39,49 @@ class __TwigTemplate_ea89abb853b6f80e609215b2e78fc793709b4f1770ec48984d777aa3d9e
         echo "<!DOCTYPE html>
 <html lang=\"en\">
 <head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Document</title>
-</head>
+    ";
+        // line 4
+        $this->displayBlock('head', $context, $blocks);
+        // line 11
+        echo "</head>
 <body>
-    
-</body>
+    <p>";
+        // line 13
+        echo twig_escape_filter($this->env, ($context["user"] ?? null), "html", null, true);
+        echo "</p>
+
+    ";
+        // line 15
+        $this->displayBlock('scipt', $context, $blocks);
+        // line 22
+        echo "</body>
 </html>";
+    }
+
+    // line 4
+    public function block_head($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 5
+        echo "        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>Document</title>
+        <link rel=\"stylesheet\" href=\"../../public/css/style.css\">
+        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">
+    ";
+    }
+
+    // line 15
+    public function block_scipt($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 16
+        echo "        <!-- JS, Popper.js, and jQuery -->
+        <script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script>
+        <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\" integrity=\"sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN\" crossorigin=\"anonymous\"></script>
+        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\" integrity=\"sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV\" crossorigin=\"anonymous\"></script>
+        <script src=\"../../public/js/script.js\"></script>
+    ";
     }
 
     public function getTemplateName()
@@ -52,9 +89,14 @@ class __TwigTemplate_ea89abb853b6f80e609215b2e78fc793709b4f1770ec48984d777aa3d9e
         return "base.html.twig";
     }
 
+    public function isTraitable()
+    {
+        return false;
+    }
+
     public function getDebugInfo()
     {
-        return array (  37 => 1,);
+        return array (  79 => 16,  75 => 15,  66 => 5,  62 => 4,  57 => 22,  55 => 15,  50 => 13,  46 => 11,  44 => 4,  39 => 1,);
     }
 
     public function getSourceContext()
@@ -62,12 +104,24 @@ class __TwigTemplate_ea89abb853b6f80e609215b2e78fc793709b4f1770ec48984d777aa3d9e
         return new Source("<!DOCTYPE html>
 <html lang=\"en\">
 <head>
-    <meta charset=\"UTF-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Document</title>
+    {% block head %}
+        <meta charset=\"UTF-8\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <title>Document</title>
+        <link rel=\"stylesheet\" href=\"../../public/css/style.css\">
+        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" integrity=\"sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z\" crossorigin=\"anonymous\">
+    {% endblock %}
 </head>
 <body>
-    
+    <p>{{ user }}</p>
+
+    {% block scipt %}
+        <!-- JS, Popper.js, and jQuery -->
+        <script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script>
+        <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\" integrity=\"sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN\" crossorigin=\"anonymous\"></script>
+        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\" integrity=\"sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV\" crossorigin=\"anonymous\"></script>
+        <script src=\"../../public/js/script.js\"></script>
+    {% endblock %}
 </body>
 </html>", "base.html.twig", "C:\\xampp\\htdocs\\annonces\\application\\templates\\base.html.twig");
     }
--- public/index.php
+++ public/index.php
@@ -1,44 +1,44 @@
 <?php
 require_once '../vendor/autoload.php';
-
-$router = new AltoRouter();
+use App\Test;
 
 //Routeur
 $uri = $_SERVER['REQUEST_URI'];
 $router = new AltoRouter();
 $router->setBasePath('../public');
 
-$router->map('GET', '/', function(){
-    echo "page d'accueil";
+$router->map('GET', '/', function () {
+    $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
+    $twig = new \Twig\Environment($loader, [
+        'cache' => '../application/cache',
+        'debug' => true,
+    ]);
+    $twig->addExtension(new \Twig\Extension\DebugExtension());
+
+    $template = $twig->load('base.html.twig');
+
+
+    $test= new test();
+
+    echo $template->render([
+        'user' => $test->user,
+        'hello' => $test->hello(),
+    ]);
 });
 
-$router->map('GET', '/contact', function(){
-    echo "page de contact";
+
+
+
+$router->map('GET', '/contact', function () {
+    echo 'page de contact';
 });
-$router->map('GET', '/post-[*:slug]-[i:id]', function($slug, $id){
+$router->map('GET', '/post-[*:slug]-[i:id]', function ($slug, $id) {
     echo "je suis sur l'annonce: $slug qui a l'index: $id";
 });
+$router->map('GET', '/[*]', function () {
+    echo "cette page n'existe pas";
+});
 $match = $router->match();
-if ($match !== null){
+if ($match !== null) {
     call_user_func_array($match['target'], $match['params']);
 }
-
-dump($router);
-dump($match);
-
-//Twig
-// $loader = new \Twig\Loader\FilesystemLoader('../application/templates');
-// $twig = new \Twig\Environment($loader, [
-
-//     'cache' => '../application/cache',
-//     'debug' => true,
-//     ]);
-//     $twig->addExtension(new \Twig\Extension\DebugExtension());
-
-// $template = $twig->load('base.html.twig');
-// echo $template->render(array(
-//     'user' => $user,
-//     'exemple' => exemple(),
-// ));
-
-// dump($_SERVER);
