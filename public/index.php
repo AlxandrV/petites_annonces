0bbbcb12374da4fb0e4a53460f3aac757b2591bf
AlxandrV <a.vigneaud@gmail.com>
2020-08-24 17:00:27 +0200

    Add - file header.twig --front

--- public/index.php
+++ public/index.php
@@ -8,17 +8,9 @@ require_once '../vendor/autoload.php';
 //     ]);
 //     $twig->addExtension(new \Twig\Extension\DebugExtension());
 
-<<<<<<< HEAD
-$template = $twig->load('base.html.twig');
-echo $template->render(array(
-    'user' => '$user',
-    // 'exemple' => exemple(),
-));
-=======
 // $template = $twig->load('base.html.twig');
 // echo $template->render(array(
 //     'user' => $user,
 //     'exemple' => exemple(),
 // ));
 dump($_SERVER);
->>>>>>> 86275453ab72a70280b574038bfb6db40dcf65cc
