cb177ca21620a28be0ef1abfdef0599feaa13200
Jeremy <g3raphacs@gmail.com>
2020-08-25 14:00:22 +0200

    merge

--- application/cache/aa/aa16e1dff8c012695492110211f42951ad8fb5cff760dc7f4ad372ae493c8213.php
+++ application/cache/aa/aa16e1dff8c012695492110211f42951ad8fb5cff760dc7f4ad372ae493c8213.php
@@ -33,6 +33,24 @@ class __TwigTemplate_04221abde1a545802dcbf1718d8ddd82b6eb2df295393a932238b33d137
     protected function doDisplay(array $context, array $blocks = [])
     {
         $macros = $this->macros;
+        // line 1
+        echo "<header>
+    <div id=\"first\" class=\"d-flex justify-content-between align-items-center\">
+        <div id=\"logo\"><img src=\"../../public/media/computer.png\" alt=\"Logo\" width=\"256\" height=\"256\"></div>
+        <button class=\"btn\">Poster une annonce</button>
+    </div>
+    <div id=\"secondary\" class=\"container d-flex justify-content-around\">
+        <input type=\"text\" name=\"search\" id=\"search\" class=\"form-control\" placeholder=\"Rechercher\">
+        <div class=\"btn-group\">
+            <button type=\"button\" class=\"btn btn-secondary dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">Catégorie</button>
+            <div class=\"dropdown-menu dropdown-menu-right\">
+              <button class=\"dropdown-item\" type=\"button\">Catégorie1</button>
+              <button class=\"dropdown-item\" type=\"button\">Catégorie2</button>
+              <button class=\"dropdown-item\" type=\"button\">Catégorie3</button>
+            </div>
+          </div>
+    </div>
+</header>";
     }
 
     public function getTemplateName()
@@ -42,11 +60,27 @@ class __TwigTemplate_04221abde1a545802dcbf1718d8ddd82b6eb2df295393a932238b33d137
 
     public function getDebugInfo()
     {
-        return array ();
+        return array (  37 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("", "header.html.twig", "C:\\xampp\\htdocs\\annonces\\application\\templates\\header.html.twig");
+        return new Source("<header>
+    <div id=\"first\" class=\"d-flex justify-content-between align-items-center\">
+        <div id=\"logo\"><img src=\"../../public/media/computer.png\" alt=\"Logo\" width=\"256\" height=\"256\"></div>
+        <button class=\"btn\">Poster une annonce</button>
+    </div>
+    <div id=\"secondary\" class=\"container d-flex justify-content-around\">
+        <input type=\"text\" name=\"search\" id=\"search\" class=\"form-control\" placeholder=\"Rechercher\">
+        <div class=\"btn-group\">
+            <button type=\"button\" class=\"btn btn-secondary dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">Catégorie</button>
+            <div class=\"dropdown-menu dropdown-menu-right\">
+              <button class=\"dropdown-item\" type=\"button\">Catégorie1</button>
+              <button class=\"dropdown-item\" type=\"button\">Catégorie2</button>
+              <button class=\"dropdown-item\" type=\"button\">Catégorie3</button>
+            </div>
+          </div>
+    </div>
+</header>", "header.html.twig", "C:\\xampp\\htdocs\\annonces\\application\\templates\\header.html.twig");
     }
 }
