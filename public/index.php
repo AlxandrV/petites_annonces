2e3381c7ea68f68b1290b2c1db78f9065c3b52bc
AlxandrV <a.vigneaud@gmail.com>
2020-08-25 14:02:19 +0200

    Conflict

--- public/index.php
+++ public/index.php
@@ -1,21 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
-<<<<<<< HEAD
-$loader = new \Twig\Loader\FilesystemLoader('../application/templates');
-$twig = new \Twig\Environment($loader, [
-
-    'cache' => '../application/cache',
-    'debug' => true,
-    ]);
-    $twig->addExtension(new \Twig\Extension\DebugExtension());
-
-$template = $twig->load('base.html.twig');
-echo $template->render(array(
-    'user' => 'user',
-    // 'exemple' => exemple(),
-));
-// dump($_SERVER);
-=======
 use App\Test;
 
 //Routeur
@@ -43,8 +27,6 @@ $router->map('GET', '/', function () {
 });
 
 
-
-
 $router->map('GET', '/contact', function () {
     echo 'page de contact';
 });
@@ -54,8 +36,9 @@ $router->map('GET', '/post-[*:slug]-[i:id]', function ($slug, $id) {
 $router->map('GET', '/[*]', function () {
     echo "cette page n'existe pas";
 });
+
+// echo getcwd();
 $match = $router->match();
 if ($match !== null) {
     call_user_func_array($match['target'], $match['params']);
 }
->>>>>>> 3237c38a895fda20feadfed2670a29e1f2041499
