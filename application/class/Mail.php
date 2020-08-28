f2c1587a8ec8bb24d6cdbff6bd2eb46317caae16
Jeremy <g3raphacs@gmail.com>
2020-08-28 11:05:38 +0200

    mail function test

--- application/class/Mail.php
+++ application/class/Mail.php
@@ -1,37 +1,56 @@
 <?php
-
 namespace App;
 
 use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\SMTP;
+use \PHPMailer\PHPMailer\Exception;
 
-class Mail {
+class Mail
+{
 
-    public static function sendEmail(){
-        //Create a new PHPMailer instance
-        $mail = new PHPMailer;
-        //Set who the message is to be sent from
-        $mail->setFrom('g3raphacs@gmail.com', 'J M');
-        //Set an alternative reply-to address
-        $mail->addReplyTo('g3raph@gmail.com', 'J M');
-        //Set who the message is to be sent to
-        $mail->addAddress('mathieu4job@gmail.com', 'Jeremy Mathieu');
-        //Set the subject line
-        $mail->Subject = 'PHPMailer test';
-        //Read an HTML message body from an external file, convert referenced images to embedded,
-        //convert HTML into a basic plain-text alternative body
-        // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-        $mail->Body = 'HELLO ! This is a plain-text message body';
-        //Replace the plain text body with one created manually
-        $mail->AltBody = 'This is a plain-text message body';
-        //Attach an image file
-        $mail->addAttachment('images/phpmailer_mini.png');
+    public static function sendEmail() {
 
-        //send the message, check for errors
-        if (!$mail->send()) {
-            echo 'Mailer Error: '. $mail->ErrorInfo;
-        } else {
-            echo 'Message sent!';
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
+
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.mail.com';                       // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'jm-dev-test@mail.com';                // SMTP username
+        $mail->Password   = 'v83VevH5Q7p3QgGPjCIB';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom('jm-dev-test@mail.com', 'Mailer');      //Set who the message is to be sent from
+        $mail->addAddress('mathieu4job@gmail.com', 'Jeremy Mathieu');     // Add a recipient
+        // $mail->addAddress('ellen@example.com');               // Name is optional
+        $mail->addReplyTo('mathieu4job@gmail.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'Message has been sent';
+
+        } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
-
     }
 }
